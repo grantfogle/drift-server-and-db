8802ad0cb1ca13fd973aa12691f3906be50ed181
grantfogle <grantjfogle@gmail.com>
2022-01-01 20:15:04 -0700

    set up first get all rivers route

---
+++ queries.js
@@ -0,0 +1,16 @@
+const db = require("./database-connection");
+
+module.exports = {
+  listAll() {
+    return db("rivers");
+  }
+  // getPlaylistByName(playlistName) {
+  //     return db('playlists').where('playlistName', playlistName);
+  // },
+  // createPlaylist(newPlaylist) {
+  //     return db('playlists').insert(newPlaylist).returning('*');
+  // },
+  // deletePlaylist(playlistName) {
+  //     return db('playlists').where('playlistName', playlistName).delete();
+  // }
+};
