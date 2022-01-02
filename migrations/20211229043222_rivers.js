bb7d44fa91f91de74a2954d338d9adaef24fe35f
grantfogle <grantjfogle@gmail.com>
2022-01-01 20:04:16 -0700

    adding seed file for knex rivers table

--- migrations/20211229043222_rivers.js
+++ migrations/20211229043222_rivers.js
@@ -1,10 +1,19 @@
 // knex migrate:latest
 exports.up = function(knex) {
-  return knex.schema.createTable("riverList", river => {
-    river.increments("river_id");
+  return knex.schema.createTable("rivers", river => {
+    river.increments("id");
+    river.string("river_name");
+    river.string("geo_identifier");
+    river.string("state");
+    river.string("country");
+    river.string("watershed");
+    river.string("usgsId");
+    river.string("lat");
+    river.string("long");
+    river.integer("currentFlow");
+    river.string("alerts");
   });
 };
-
 // knex migrate:rollback
 exports.down = function(knex) {
   return knex.schema.dropTableIfExists("rivers");
